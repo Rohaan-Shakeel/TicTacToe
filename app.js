1e12af042845db901bebb8d1f9b645f6f1bb07b6
Rohaan-Shakeel <74498559+Rohaan-Shakeel@users.noreply.github.com>
2020-11-15 20:04:47 +0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,70 @@
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+const cellElements = document.querySelectorAll('[data-cell]')
+let circleTurn 
+const draw = function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+        alert("The game is a draw")
+    })
+}
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+const WINNING_COMBINATIONS = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6], 
+]
+cellElements.forEach(cell => {
+    cell.addEventListener('click', handleClick, { once: true })
+})
+
+function handleClick(e) {
+    const cell = e.target
+    const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+    placeMark(cell, currentClass)
+    if (checkWin(currentClass)) {
+        if(circleTurn) {
+            alert("Circle has Won")
+        } else {
+            alert("X has won")
+        } 
+    
+    }
+    if (checkWin === isDraw()) {
+        alert("The game is a draw")
+    }
+    //Check for win
+    //Check for draw
+    //Switch turn
+    swapTurns()
+}
+
+function isDraw() {
+    return [...cellElements].every(cell => {
+        return cell.classList.contains(X_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+        alert("The game is a draw")
+    })
+}
+
+function placeMark(cell, currentClass) {
+    cell.classList.add(currentClass)
+}
+
+function swapTurns() {
+    circleTurn = !circleTurn
+}
+
+function checkWin(currentClass) {
+   return WINNING_COMBINATIONS.some(combination => {
+       return combination.every(index => {
+           return cellElements[index].classList.contains(currentClass)
+       })
+   })
+}
+
+    
